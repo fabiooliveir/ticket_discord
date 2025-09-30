640b528ef9914cf0f818ed1d4db653fcf3da58a2
fabiooliveir <fboliveiran@gmail.com>
2025-09-30 09:40:41 -0300

    migration: add clientId to tickets

---
+++ src/database/migrations/1703000000005-AddClientIdToTickets.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddClientIdToTickets1703000000005 implements MigrationInterface {
+  name = 'AddClientIdToTickets1703000000005';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      "ALTER TABLE `tickets` ADD `clientId` varchar(100) NULL",
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      "ALTER TABLE `tickets` DROP COLUMN `clientId`",
+    );
+  }
+}
+
+
